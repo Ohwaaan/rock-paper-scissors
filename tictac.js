bc7e69b6e9a78f5f7173bb450b580ea951da135c
Ohwaaan <owenlian88@gmail.com>
2023-07-25 00:50:44 -0400

    added player, computer, single and multi rounds

--- tictac.js
+++ tictac.js
@@ -1,1 +1,56 @@
-console.log('itworks!')
+let getComputerChoice = () => {
+//return 'rock', 'paper', 'or scissors'
+    let random = Math.floor(Math.random()*3);
+    if (random === 0) 
+        return 'rock';
+    else if(random === 1)
+        return 'paper';
+    else 
+        return 'scissors';
+}
+
+let getPlayerSelection = () => {
+    let selection = prompt('Enter rock, paper or scissors!')
+    selection = selection.toLowerCase();
+
+    while (! (selection === 'rock' || selection === 'paper' || selection === 'scissors'))
+        selection = prompt('That was invalid, please enter rock, paper or scissors!')
+    return selection;
+}
+
+let singleRound = (playerSelection, computerSelection) => {
+    let result;
+
+    if (playerSelection === computerSelection){
+        result = 'Tie';
+    }
+    else if(playerSelection = 'rock'){
+        if (computerSelection = 'scissors')
+            result = 'You win! Rock beats scissors!';
+        else    
+            result = 'You lose! Paper beats rock!';
+    }
+    else if(playerSelection = 'scissors'){
+        if (computerSelection = 'paper')
+            result = 'You win! Scissors beats paper!';
+        else
+            result = 'You lose! Rock beats scissor!'; 
+    }
+    else{
+        if(computerSelection = 'rock')
+            result = 'You win! Paper beats rock!';
+        else   
+            result = 'You lose! Scissors beat paper!';
+    }
+        return result;
+}
+
+let game = () => {
+    for(let i = 0; i<5; i++)
+    {
+        choice = getPlayerSelection();
+        console.log(singleRound(choice,getComputerChoice()));
+    }
+}
+
+game();
