5a47b7bd3e32b26e17c8f0ed65ed87e127bbf085
Ohwaaan <owenlian88@gmail.com>
2024-03-22 00:12:17 -0400

    added victory at 5 wins rule

--- rps.js
+++ rps.js
@@ -9,11 +9,17 @@ let getComputerChoice = () =>{
 let resultMessage = (result, playerChoice, computerChoice) =>
 {
     if (result === 'tie')
-    { printResult('tie') }
+    { printResultMessage('tie') }
     else if(result === 'win')
-    { printResult('You win! '+ playerChoice + ' beats ' + computerChoice + '!'); }
+    { 
+        updateScore('win');
+        printResultMessage('You win! '+ playerChoice + ' beats ' + computerChoice + '! ' + 'score: ' + playerScore + ' vs ' + computerScore); 
+    }
     else if(result === 'lose')
-    { printResult('You lose! '+ playerChoice + ' loses to ' + computerChoice + '!'); }
+    { 
+        updateScore('lose');
+        printResultMessage('You lose! '+ playerChoice + ' loses to ' + computerChoice + '! ' + 'score: '+ playerScore + ' vs ' + computerScore); 
+    }
     else
     { console.log('send the right thing! >:(')}
 }
@@ -34,19 +40,42 @@ let playRound = (playerChoice) =>{
     let result = gameLogic(playerChoice,computerChoice);
     
     resultMessage(result,playerChoice,computerChoice);
-    test();
+    winOrlose();
 }
 
-let printResult = (result) =>{
+let printResultMessage = (resultMessage) =>{
     const content = document.createElement('div');
-    content.textContent = result;
+    content.textContent = resultMessage;
     results.appendChild(content);
+}
+
+let updateScore = (result) =>{
+    if (result === 'win')
+    { playerScore +=1; }
+    else
+    { computerScore +=1; }  
+}
+
+let winOrlose = ()=>{
+    if (playerScore === 5){
+        printResultMessage('OMG YOURE THE VICTOR YOU GOT 5 WINS!!! The game will now reset!')
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if(computerScore === 5)
+    {
+        printResultMessage('damn dog u suck try again')
+        playerScore = 0;
+        computerScore = 0;
+    }
 }
 
 let rock     = document.querySelector('.rock');
 let paper    = document.querySelector('.paper');
 let scissors = document.querySelector('.scissors');
 let results  = document.querySelector('.results');
+let playerScore = 0;
+let computerScore = 0;
 
 rock.addEventListener('click',function(){playRound('rock')})
 paper.addEventListener('click',function(){playRound('paper')})
